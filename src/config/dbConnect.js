d4930439e2b3632f684b9c98de520e1317eb9b06
Inguim <igorazevedost@gmail.com>
2023-12-17 15:05:46 -0300

    feat: moongo connection

---
+++ src/config/dbConnect.js
@@ -0,0 +1,8 @@
+import moongose, { mongo } from "mongoose";
+
+async function conectaNaDatabase() {
+  moongose.connect(process.env.DB_CONECTION_STRING);
+  return moongose.connection;
+}
+
+export default conectaNaDatabase;
