41628fbb682423fadb93a25ca8ed7e317900c099
Inguim <igorazevedost@gmail.com>
2023-12-17 16:56:33 -0300

    feat: autor schema

---
+++ src/models/Autor.js
@@ -0,0 +1,12 @@
+import moongose from "mongoose";
+
+const autorSchema = new moongose.Schema({
+    id: { type: moongose.Schema.Types.ObjectId },
+    nome: { type: String, required: true },
+    nacionalidade: { type: String },
+}, { versionKey: false });
+
+
+const autor = moongose.model("autores", autorSchema);
+
+export { autor, autorSchema };
