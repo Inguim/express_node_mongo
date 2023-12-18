88d7845197aa3a5f22cd4cbbd82be7c66c0bee9c
Inguim <igorazevedost@gmail.com>
2023-12-17 21:38:14 -0300

    feat gerenciador de erro

--- src/middlewares/errorHandling.js
+++ src/middlewares/errorHandling.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-unused-vars */
 import mongoose from "mongoose";
+import BaseError from "../erros/BaseError.js";
+import InvalidRequestError from "../erros/InvalidRequestError.js";
+import ValidationError from "../erros/ValidationError.js";
 
 function errorHandling(erro, req, res, next) {
   console.log("Erro:", erro.message);
-  if (erro instanceof mongoose.Error.CastError) {
-    return res
-      .status(400)
-      .send({ message: "Um ou mais dados fornecidos estão incorretos." });
-  }
-  return res.status(500).send({ message: "Erro interno de servidor." });
+  if (erro instanceof mongoose.Error.CastError) return new InvalidRequestError().sendResponse(res);
+  if (erro instanceof mongoose.Error.ValidationError) return new ValidationError(erro).sendResponse(res);
+  return new BaseError().sendResponse(res);
 }
 
 export default errorHandling;
